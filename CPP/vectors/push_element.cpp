61cddec8b400a135d31fbcb0982f27b6cb8d747d
Munagala Sai Raghava <88725496+sairaghavamunagala@users.noreply.github.com>
2024-09-24 23:39:45 +0530

    Added push_element.cpp

---
+++ CPP/vectors/push_element.cpp
@@ -0,0 +1,10 @@
+// Adding elements to the vector
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> vect;
+    vect.push_back(1);
+    vect.push_back(5);
+    return 0;
+}
